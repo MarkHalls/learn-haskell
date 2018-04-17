731fc11f358ccbc0437a376347eae3cbd4825f75
Mark Halls <firesetterdev@gmail.com>
2018-04-17 09:23:04 -0700

    Completed Chapter 4 Section 5: Case Expressions

---
+++ tutorial/baby.hs
@@ -0,0 +1,10 @@
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list"
+maximum' [x] = x
+maximum' (x:xs) = max x (maximum' xs)
+
+
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+  | n <= 0 = []
+  | otherwise = x:replicate' (n-1) x
